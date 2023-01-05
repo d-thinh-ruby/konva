04c7d563f0b12905188b5b0d04304d6c17a1c835
Dang Minh Thinh <d-thinh@ruby-dev.vn>
2023-01-05 17:15:22 +0700

    Add empty page

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { loadFonts } from "../functions/load-fonts";
+import emptyFolder from "../assets/empty-folder.svg";
 
 const App = () => {
   const data = Object.entries(localStorage);
@@ -11,21 +12,25 @@ const App = () => {
   return (
     <div className="row">
       <h1 className="d-flex justify-content-center">Art List</h1>
-      <div className="col-6 offset-3">
-        <div className="list-group">
-          {data.map(([key, _]) => {
-            return (
-              <button
-                key={key}
-                type="button"
-                className="list-group-item list-group-item-action"
-                aria-current="true"
-              >
-                <Link to={`art-board/${key}`}>{key}</Link>
-              </button>
-            );
-          })}
-        </div>
+      <div className="col-6 offset-3 text-center">
+        {data.length ? (
+          <div className="list-group">
+            {data.map(([key, _]) => {
+              return (
+                <button
+                  key={key}
+                  type="button"
+                  className="list-group-item list-group-item-action"
+                  aria-current="true"
+                >
+                  <Link to={`art-board/${key}`}>{key}</Link>
+                </button>
+              );
+            })}
+          </div>
+        ) : (
+          <img src={emptyFolder} alt="" width={200}/>
+        )}
       </div>
     </div>
   );
